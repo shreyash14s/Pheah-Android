2effb5f1b3a0bb7b8df2cbe1fc6863374328072b
Shreyash S Sarnayak <shreyash14s@gmail.com>
2015-10-12 23:14:17 +0530

    Updated to AppCompatActivity. From ActionBarActivity

--- app/src/main/java/com/pheah/pheah_android/MapActivity.java
+++ app/src/main/java/com/pheah/pheah_android/MapActivity.java
@@ -1,12 +1,12 @@
 package com.pheah.pheah_android;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MapActivity extends ActionBarActivity {
+public class MapActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
