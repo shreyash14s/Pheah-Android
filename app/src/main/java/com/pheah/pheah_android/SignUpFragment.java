d1d76011634ee8df0b9b223e15e44f0279d42806
Shreyash S Sarnayak <shreyash14s@gmail.com>
2015-10-28 00:01:33 +0530

    Added Google Plus Plugin. Changed Activity to Fragments.

--- app/src/main/java/com/pheah/pheah_android/SignUpFragment.java
+++ app/src/main/java/com/pheah/pheah_android/SignUpFragment.java
@@ -1,140 +1,127 @@
 package com.pheah.pheah_android;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 
-	public static final String PREF = "com.pheah.pheah_android.PREF";
-
-	private Fragment fragment;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.frag_container);
-
-		fragment = new SignUpFragment();
-
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		transaction.add(R.id.fragmentContainer, fragment);
-		transaction.commit();
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		fragment.onActivityResult(requestCode, resultCode, data);
-	}
-
-	public void changeFragment(Fragment fragment) {
-		this.fragment = fragment;
-
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		transaction.replace(R.id.fragmentContainer, fragment);
-		transaction.addToBackStack(null);
-		transaction.commit();
-	}
-
-	/*public void openMap(MapFragment f) {
-		this.fragment = f;
-
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		transaction.replace(R.id.fragmentContainer, fragment);
-		transaction.commit();
-
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		getSupportActionBar().setHomeButtonEnabled(true);
-	}*/
-}
-/*
-public class MainActivity extends AppCompatActivity implements
+public class SignUpFragment extends Fragment implements
 		View.OnClickListener,
 		ActivityCompat.OnRequestPermissionsResultCallback,
 		GoogleApiClient.ConnectionCallbacks,
 		GoogleApiClient.OnConnectionFailedListener {
 
-	private static final String TAG = "pheah.MainActivity";
+	public static final String ARG_SIGN_UP = "sign_up";
 
-	*/
-/* RequestCode for resolutions involving sign-in *//*
+	private static final String TAG = "pheah.SignUpFragment";
 
+	/* RequestCode for resolutions involving sign-in */
 	private static final int RC_SIGN_IN = 1;
 
-	*/
-/* RequestCode for resolutions to get GET_ACCOUNTS permission on M *//*
-
+	/* RequestCode for resolutions to get GET_ACCOUNTS permission on M */
 	private static final int RC_PERM_GET_ACCOUNTS = 2;
 
-	*/
-/* Keys for persisting instance variables in savedInstanceState *//*
-
+	/* Keys for persisting instance variables in savedInstanceState */
 	private static final String KEY_IS_RESOLVING = "is_resolving";
 	private static final String KEY_SHOULD_RESOLVE = "should_resolve";
 
-	*/
-/* Client for accessing Google APIs *//*
-
+	/* Client for accessing Google APIs */
 	private GoogleApiClient mGoogleApiClient;
 
-	*/
-/* View to display current status (signed-in, signed-out, disconnected, etc) *//*
-
+	/* View to display current status (signed-in, signed-out, disconnected, etc) */
 	private TextView mStatus;
 
 	// [START resolution_variables]
-	*/
-/* Is there a ConnectionResult resolution in progress? *//*
-
+	/* Is there a ConnectionResult resolution in progress? */
 	private boolean mIsResolving = false;
 
-	*/
-/* Should we automatically resolve ConnectionResults when possible? *//*
-
+	/* Should we automatically resolve ConnectionResults when possible? */
 	private boolean mShouldResolve = false;
 	// [END resolution_variables]
 
-	public static boolean startMap = true;
+	private boolean startMap = true;
+
+	public SignUpFragment() {
+		// Required empty public constructor
+	}
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.layout_signup, container, false);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.layout_signup);
+
+		Bundle arguments = getArguments();
+		if (arguments!= null)
+			startMap = arguments.getBoolean(ARG_SIGN_UP, true);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
 
 		// Restore from saved instance state
 		// [START restore_saved_instance_state]
 		if (savedInstanceState != null) {
 			mIsResolving = savedInstanceState.getBoolean(KEY_IS_RESOLVING);
 			mShouldResolve = savedInstanceState.getBoolean(KEY_SHOULD_RESOLVE);
+			//startMap = savedInstanceState.getBoolean(ARG_SIGN_UP, true);
 		}
 		// [END restore_saved_instance_state]
 
 		// Set up button click listeners
-		findViewById(R.id.sign_in).setOnClickListener(this);
-		findViewById(R.id.sign_out).setOnClickListener(this);
-		findViewById(R.id.disconnect_button).setOnClickListener(this);
-		findViewById(R.id.map_button).setOnClickListener(new View.OnClickListener() {
+		getActivity().findViewById(R.id.sign_in).setOnClickListener(this);
+		getActivity().findViewById(R.id.sign_out).setOnClickListener(this);
+		getActivity().findViewById(R.id.disconnect_button).setOnClickListener(this);
+		getActivity().findViewById(R.id.map_button).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				startActivity(new Intent(MainActivity.this, MapActivity.class)); //TODO: Make Fragment.
+				//startActivity(new Intent(getActivity(), MapActivity.class)); //TODO: Make Fragment.
+				//((MainActivity) getActivity()).openMap(new MapFragment());
 			}
 		});
 
 		// Large sign-in
-		((SignInButton) findViewById(R.id.sign_in)).setSize(SignInButton.SIZE_WIDE);
+		((SignInButton) getActivity().findViewById(R.id.sign_in)).setSize(SignInButton.SIZE_WIDE);
 
 		// Start with sign-in button disabled until sign-in either succeeds or fails
-		findViewById(R.id.sign_in).setEnabled(false);
+		getActivity().findViewById(R.id.sign_in).setEnabled(false);
 
 		// Set up view instances
-		mStatus = (TextView) findViewById(R.id.status);
+		mStatus = (TextView) getActivity().findViewById(R.id.status);
 
 		// [START create_google_api_client]
 		// Build GoogleApiClient with access to basic profile
-		mGoogleApiClient = new GoogleApiClient.Builder(this)
+		mGoogleApiClient = new GoogleApiClient.Builder(getContext())
 				.addConnectionCallbacks(this)
 				.addOnConnectionFailedListener(this)
 				.addApi(Plus.API)
@@ -165,41 +152,38 @@ public class MainActivity extends AppCompatActivity implements
 			}
 
 			// Set button visibility
-			findViewById(R.id.sign_in).setVisibility(View.GONE);
-			//findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
-			findViewById(R.id.sign_out).setVisibility(View.VISIBLE);
-			findViewById(R.id.disconnect_button).setVisibility(View.VISIBLE);
+			getActivity().findViewById(R.id.sign_in).setVisibility(View.GONE);
+			//getActivity().findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+			getActivity().findViewById(R.id.sign_out).setVisibility(View.VISIBLE);
+			getActivity().findViewById(R.id.disconnect_button).setVisibility(View.VISIBLE);
 		} else {
 			// Show signed-out message and clear email field
 			mStatus.setText("Please Sign in");
-			//((TextView) findViewById(R.id.email)).setText("");
+			//((TextView) getActivity().findViewById(R.id.email)).setText("");
 
 			// Set button visibility
-			findViewById(R.id.sign_in).setEnabled(true);
-			findViewById(R.id.sign_in).setVisibility(View.VISIBLE);
-			//findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
-			findViewById(R.id.sign_out).setVisibility(View.GONE);
-			findViewById(R.id.disconnect_button).setVisibility(View.GONE);
+			getActivity().findViewById(R.id.sign_in).setEnabled(true);
+			getActivity().findViewById(R.id.sign_in).setVisibility(View.VISIBLE);
+			//getActivity().findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+			getActivity().findViewById(R.id.sign_out).setVisibility(View.GONE);
+			getActivity().findViewById(R.id.disconnect_button).setVisibility(View.GONE);
 		}
 	}
 
-	*/
-/**
+	/**
 	 * Check if we have the GET_ACCOUNTS permission and request it if we do not.
 	 * @return true if we have the permission, false if we do not.
-	 *//*
-
+	 */
 	private boolean checkAccountsPermission() {
 		final String perm = Manifest.permission.GET_ACCOUNTS;
-		int permissionCheck = ContextCompat.checkSelfPermission(this, perm);
+		int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), perm);
 		if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
 			// We have the permission
 			return true;
-		} */
-/*else if (ActivityCompat.shouldShowRequestPermissionRationale(this, perm)) {
+		} /*else if (ActivityCompat.shouldShowRequestPermissionRationale(this, perm)) {
 			// Need to show permission rationale, display a snackbar and then request
 			// the permission again when the snackbar is dismissed.
-			Snackbar.make(findViewById(R.id.main_layout),
+			Snackbar.make(getActivity().findViewById(R.id.main_layout),
 					R.string.contacts_permission_rationale,
 					Snackbar.LENGTH_INDEFINITE)
 					.setAction(android.R.string.ok, new View.OnClickListener() {
@@ -218,10 +202,9 @@ public class MainActivity extends AppCompatActivity implements
 					new String[]{perm},
 					RC_PERM_GET_ACCOUNTS);
 			return false;
-		}*//*
-
+		}*/
 		else {
-			Toast.makeText(this, "Please grant all the required permissions", Toast.LENGTH_SHORT).show();
+			Toast.makeText(getActivity(), "Please grant all the required permissions", Toast.LENGTH_SHORT).show();
 			return false;
 		}
 	}
@@ -234,15 +217,16 @@ public class MainActivity extends AppCompatActivity implements
 		Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
 		Log.d(TAG, "p = " + currentPerson + ", m = " + startMap);
 		if (currentPerson != null) {
-			SharedPreferences p = getSharedPreferences(PREF, MODE_PRIVATE);
+			SharedPreferences p = getActivity().getSharedPreferences(PREF, Context.MODE_PRIVATE);
 			SharedPreferences.Editor e = p.edit();
 			e.putString("name", currentPerson.getDisplayName());
 			e.putString("email", Plus.AccountApi.getAccountName(mGoogleApiClient));
 			e.putString("photoUrl", currentPerson.getImage().getUrl());
 			e.apply();
 			if (startMap) {
-				startActivity(new Intent(this, MapActivity.class)); //TODO: Make Fragment.
-				this.finish();
+				startActivity(new Intent(getActivity(), MapActivity.class)); //TODO: Make Fragment.
+				getActivity().finish();
+				//((MainActivity) getActivity()).openMap(new MapFragment());
 			}
 		}
 	}
@@ -253,13 +237,13 @@ public class MainActivity extends AppCompatActivity implements
 
 	// [START on_start_on_stop]
 	@Override
-	protected void onStart() {
+	public void onStart() {
 		super.onStart();
 		mGoogleApiClient.connect();
 	}
 
 	@Override
-	protected void onStop() {
+	public void onStop() {
 		super.onStop();
 		mGoogleApiClient.disconnect();
 	}
@@ -267,7 +251,7 @@ public class MainActivity extends AppCompatActivity implements
 
 	// [START on_save_instance_state]
 	@Override
-	protected void onSaveInstanceState(Bundle outState) {
+	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		outState.putBoolean(KEY_IS_RESOLVING, mIsResolving);
 		outState.putBoolean(KEY_SHOULD_RESOLVE, mShouldResolve);
@@ -282,7 +266,7 @@ public class MainActivity extends AppCompatActivity implements
 
 		if (requestCode == RC_SIGN_IN) {
 			// If the error resolution was not successful we should not resolve further.
-			if (resultCode != RESULT_OK) {
+			if (resultCode != Activity.RESULT_OK) {
 				mShouldResolve = false;
 			}
 
@@ -294,7 +278,7 @@ public class MainActivity extends AppCompatActivity implements
 
 	@Override
 	public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
-										   @NonNull int[] grantResults) {
+	                                       @NonNull int[] grantResults) {
 		Log.d(TAG, "onRequestPermissionsResult:" + requestCode);
 		if (requestCode == RC_PERM_GET_ACCOUNTS) {
 			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -327,8 +311,6 @@ public class MainActivity extends AppCompatActivity implements
 		Log.w(TAG, "onConnectionSuspended:" + i);
 	}
 
-	private ConnectionResult mConnectionResult;
-
 	// [START on_connection_failed]
 	@Override
 	public void onConnectionFailed(ConnectionResult connectionResult) {
@@ -341,7 +323,7 @@ public class MainActivity extends AppCompatActivity implements
 			Log.d(TAG, "inside first if");
 			if (connectionResult.hasResolution()) {
 				try {
-					connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+					connectionResult.startResolutionForResult(getActivity(), RC_SIGN_IN);
 					mIsResolving = true;
 					Log.d(TAG, "inside try and second if");
 				} catch (IntentSender.SendIntentException e) {
@@ -360,20 +342,16 @@ public class MainActivity extends AppCompatActivity implements
 			// Show the signed-out UI
 			showSignedOutUI();
 		}
-
-		// Save the result and resolve the connection failure upon a user click.
-		mConnectionResult = connectionResult;
-		Log.d(TAG, "Saving result.");
 	}
 	// [END on_connection_failed]
 
 	private void showErrorDialog(ConnectionResult connectionResult) {
 		GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-		int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+		int resultCode = apiAvailability.isGooglePlayServicesAvailable(getActivity());
 
 		if (resultCode != ConnectionResult.SUCCESS) {
 			if (apiAvailability.isUserResolvableError(resultCode)) {
-				apiAvailability.getErrorDialog(this, resultCode, RC_SIGN_IN,
+				apiAvailability.getErrorDialog(getActivity(), resultCode, RC_SIGN_IN,
 						new DialogInterface.OnCancelListener() {
 							@Override
 							public void onCancel(DialogInterface dialog) {
@@ -384,7 +362,7 @@ public class MainActivity extends AppCompatActivity implements
 			} else {
 				Log.w(TAG, "Google Play Services Error:" + connectionResult);
 				String errorString = apiAvailability.getErrorString(resultCode);
-				Toast.makeText(this, errorString, Toast.LENGTH_SHORT).show();
+				Toast.makeText(getActivity(), errorString, Toast.LENGTH_SHORT).show();
 
 				mShouldResolve = false;
 				showSignedOutUI();
@@ -448,4 +426,3 @@ public class MainActivity extends AppCompatActivity implements
 	}
 	// [END on_disconnect_clicked]
 }
-*/
